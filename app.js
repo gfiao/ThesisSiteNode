7d6f9bb91b96e773deae22bf02c5f4a8c6e4fc31
gfiao <g.fiao@campus.fct.unl.pt>
2016-07-18 15:53:54 +0100

    no socket

--- app.js
+++ app.js
@@ -126,14 +126,14 @@ function logEmotions(emotion, timestamp) {
     connection.execSql(request);
 }
 
-var io = require('socket.io').listen(1000); // initiate socket.io server
-io.sockets.on('connection', function (socket) {
-    socket.emit('news', {hello: 'world'}); // Send data to client
-
-    // wait for the event raised by the client
-    socket.on('my other event', function (data) {
-
-        console.log(data);
-        logEmotions(data[0], data[1]);
-    });
-});
+// var io = require('socket.io').listen(80); // initiate socket.io server
+// io.sockets.on('connection', function (socket) {
+//     socket.emit('news', {hello: 'world'}); // Send data to client
+//
+//     // wait for the event raised by the client
+//     socket.on('my other event', function (data) {
+//
+//         console.log(data);
+//         logEmotions(data[0], data[1]);
+//     });
+// });
