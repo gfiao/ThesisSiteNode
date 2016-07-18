d0007951edc05a3ffa517462ae8ebd25120a2eca
gfiao <g.fiao@campus.fct.unl.pt>
2016-07-18 17:18:40 +0100

    .

--- app.js
+++ app.js
@@ -61,72 +61,73 @@ app.use(function (err, req, res, next) {
 module.exports = app;
 
 
-var Connection = require('tedious').Connection;
-var config = {
-    userName: 'gfiao',
-    password: 'Leonardogui1.',
-    server: 'sharedemotions.database.windows.net',
-    // If you are on Microsoft Azure, you need this:
-    options: {encrypt: true, database: 'emotions'}
-};
-var connection = new Connection(config);
-connection.on('connect', function (err) {
-    // If no error, then good to proceed.
-    console.log("Connected");
-    // logEmotions('anger');
-    // queryDB();
-});
-
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
-
-function queryDB() {
-    request = new Request("select * from shared_emotions", function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    var result = "";
-    request.on('row', function (columns) {
-        columns.forEach(function (column) {
-            if (column.value === null) {
-                console.log('NULL');
-            } else {
-                result += column.value + " ";
-            }
-        });
-        console.log(result);
-        result = "";
-    });
-
-    request.on('done', function (rowCount, more) {
-        console.log(rowCount + ' rows returned');
-    });
-    connection.execSql(request);
-}
-
-function logEmotions(emotion, timestamp) {
-    request = new Request("insert shared_emotions(emotion, timestamp) values (@emotion, @timestamp);", function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-
-    request.addParameter('emotion', TYPES.NVarChar, emotion);
-    request.addParameter('timestamp', TYPES.Int, timestamp);
-    request.on('row', function (columns) {
-        columns.forEach(function (column) {
-            if (column.value === null) {
-                console.log('NULL');
-            } else {
-                console.log("Product id of inserted item is " + column.value);
-            }
-        });
-    });
-    connection.execSql(request);
-}
+// var Connection = require('tedious').Connection;
+// var config = {
+//     userName: 'gfiao',
+//     password: 'Leonardogui1.',
+//     server: 'sharedemotions.database.windows.net',
+//     // If you are on Microsoft Azure, you need this:
+//     options: {encrypt: true, database: 'emotions'}
+// };
+// var connection = new Connection(config);
+// connection.on('connect', function (err) {
+//     // If no error, then good to proceed.
+//     console.log("Connected");
+//     // logEmotions('anger');
+//     // queryDB();
+// });
+//
+// var Request = require('tedious').Request;
+// var TYPES = require('tedious').TYPES;
+//
+// function queryDB() {
+//     request = new Request("select * from shared_emotions", function (err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//     });
+//     var result = "";
+//     request.on('row', function (columns) {
+//         columns.forEach(function (column) {
+//             if (column.value === null) {
+//                 console.log('NULL');
+//             } else {
+//                 result += column.value + " ";
+//             }
+//         });
+//         console.log(result);
+//         result = "";
+//     });
+//
+//     request.on('done', function (rowCount, more) {
+//         console.log(rowCount + ' rows returned');
+//     });
+//     connection.execSql(request);
+// }
+//
+// function logEmotions(emotion, timestamp) {
+//     request = new Request("insert shared_emotions(emotion, timestamp) values (@emotion, @timestamp);", function (err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//     });
+//
+//     request.addParameter('emotion', TYPES.NVarChar, emotion);
+//     request.addParameter('timestamp', TYPES.Int, timestamp);
+//     request.on('row', function (columns) {
+//         columns.forEach(function (column) {
+//             if (column.value === null) {
+//                 console.log('NULL');
+//             } else {
+//                 console.log("Product id of inserted item is " + column.value);
+//             }
+//         });
+//     });
+//     connection.execSql(request);
+// }
 
 // var io = require('socket.io').listen(80); // initiate socket.io server
+//
 // io.sockets.on('connection', function (socket) {
 //     socket.emit('news', {hello: 'world'}); // Send data to client
 //
